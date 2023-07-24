9ce7f94f99f078cd57c43002db5f44b767a1612f
Elyse Kanagaratnam <elyse@hey.com>
2023-07-23 19:33:35 -0700

    added error handling, updated mobile styling

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
+const checkRequiredField = (fields) => {
+    return fields.some((field) => field.checked);
+}
+
 const form = document.querySelector('form');
-const ratingContainer = document.querySelector('#rating-container');
-const thanksContainer = document.querySelector('#ty-container');
-const confMsg = document.querySelector('.conf-msg');
-
-
 form.addEventListener('submit', (e) => {
     e.preventDefault();
+
+    const inputs = Array.from((form.querySelectorAll('input[type="radio"]')));
+    if (!checkRequiredField(inputs)) {
+        return alert('Please select a rating');
+    }
+
+    const confMsg = document.querySelector('.conf-msg');
     const data = new FormData(form);
     for (const entry of data) {
         confMsg.innerText = `You selected ${entry[1]} out of 5`;
     }
+
+    const ratingContainer = document.querySelector('#rating-container');
+    const thanksContainer = document.querySelector('#ty-container');
     ratingContainer.classList.add('hidden');
     thanksContainer.classList.remove('hidden');
 });
+
